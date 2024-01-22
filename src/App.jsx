3716255c3e82fc1ba8b5a34a0823b9ec6daa5d75
freetimezp <kraftukrnet@ukr.net>
2024-01-22 21:11:41 +0200

    02. js-travel-13-space. Navbar Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Navbar from './components/Navbar';
+
+import bgVideo from '../src/assets/earth-bg.mp4';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className='h-[700px] relative'>
+        <video autoPlay muted loop className='fixed right-0 top-0 h-[700px] w-full
+          object-cover z-[-1]'>
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
